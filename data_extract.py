131c5d0572d914ba8a3b12fbdc840e66642c80e2
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-09-05 21:40:02 +0100

    New table added to DB

--- data_extract.py
+++ data_extract.py
@@ -1,14 +1,34 @@
-import sqlite3 as sql
+import sqlite3
 
-sql_db = sql.connect("15.0.2000.5")
+# Initialize serverless database and cursor to use PostgreSQL queries
+connection = sqlite3.connect("solarweather.db")
+curs = connection.cursor()
 
-
-print(
-    sql_db.cursor(
-        """
-USE SQLCookbookSB;
-
-SELECT * FROM debt;
+# Create table
+curs.execute(
+    """
+CREATE TABLE weather(
+    name, 
+    datetime, 
+    tempmax, 
+    tempmin, 
+    temp,
+    dew,
+    humidity,
+    precip,
+    precipcover,
+    preciptype,
+    snowdept,
+    windspeed,
+    cloudcover,
+    visibility,
+    solarradiation,
+    solarenergy,
+    uvindex,
+    severerisk,
+    sunrise,
+    sunset,
+    moonphase
+)
 """
-    )
 )
