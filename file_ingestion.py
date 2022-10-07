06e2577d407a5e6c112de4ca0101fd3f2271067b
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-10-07 01:14:57 +0100

    Creating function for file injestion

--- file_ingestion.py
+++ file_ingestion.py
@@ -1,22 +1,49 @@
-from nturl2path import url2pathname
+import csv
+import codecs
+import urllib.request
+import urllib.error
+import ssl
+import sys
+
+BASEURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+APIKEY = ""
+UNITGROUP = "metric"
+FILETYPE = "csv"
+INTERVAL = "days"
 
 
-def file_ingestion(addr):
+def file_ingestion(loc, startdate="", enddate=""):
 
-    address = str(addr)
+    location = str(loc)
+    api_query = BASEURL + location
 
+    if len(UNITGROUP):
+        api_query += "&unitGroup=" + UNITGROUP
+
+    if len(FILETYPE):
+        api_query += "&contentType=" + FILETYPE
+
+    if len(INTERVAL):
+        api_query += "&include=" + INTERVAL
+
+    api_query += "&key=" + APIKEY
+
+    print(f"running query for ({api_query})")
+
+    gcontext = ssl.SSLContext()
+    info = urllib.request.urlopen(api_query, context=gcontext)
+    return info
     # Replace with some kind of regex to decide if its a postcode
 
     # Check if information already exists in database
 
-    if len(addr) > 7:
-        return None
-    else:
-
-        return None
+    # if len(addr) > 7:
+    #    return None
+    # else:
+    #    return None
 
 
 if __name__ == "__main__":
-    address_pc = "LS10 1EU"
+    location = "London"
     address_full = "46 Victoria Riverside, Leeds"
-    file_ingestion(address_pc)
+    file_ingestion(loc=location)
