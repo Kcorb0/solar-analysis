cba072ae49d64c4a7285ca81fb013f968d9c63ba
Kcorb0 <Josh_Blackmore12@hotmail.co.uk>
2022-10-08 16:13:33 +0100

    2 functions added, one to retrieve the api data with a built query. Another to output the data locally to csv format

--- file_ingestion.py
+++ file_ingestion.py
@@ -11,39 +11,60 @@ UNITGROUP = "metric"
 FILETYPE = "csv"
 INTERVAL = "days"
 
-
-def file_ingestion(loc, startdate="", enddate=""):
+# Returns the request data from the visualcrossing api based
+# on the location and optional start / end dates
+# Note: No start/end date will return the forecast
+def get_files(loc, startdate="", enddate=""):
 
     location = str(loc)
-    api_query = BASEURL + location
+    api_query = BASEURL + location + '?'
 
-    if len(UNITGROUP):
+    if len(UNITGROUP) > 0:
         api_query += "&unitGroup=" + UNITGROUP
 
-    if len(FILETYPE):
+    if len(FILETYPE) > 0:
         api_query += "&contentType=" + FILETYPE
 
-    if len(INTERVAL):
+    if len(INTERVAL) > 0:
         api_query += "&include=" + INTERVAL
 
     api_query += "&key=" + APIKEY
 
     print(f"running query for ({api_query})")
 
-    gcontext = ssl.SSLContext()
-    info = urllib.request.urlopen(api_query, context=gcontext)
-    return info
-    # Replace with some kind of regex to decide if its a postcode
 
-    # Check if information already exists in database
+    # Try to return the query output, check that the the
+    # query is valid.
+    try:
+        gcontext = ssl.SSLContext()
+        info = urllib.request.urlopen(api_query, context=gcontext)
+        return info
+    except urllib.error.HTTPError as e:
+        error_info = e.read().decode()
+        print('Error code: ', e.code, error_info)
+        sys.exit()
+    except urllib.error.URLError as e:
+        error_info = e.read().decode()
+        print('Error code: ', e.code, error_info)
+        sys.exit()
 
-    # if len(addr) > 7:
-    #    return None
-    # else:
-    #    return None
-
+# Used to parse the output data from the api call
+# to a csv format.
+def file_ingestion(file):
+    csv_text = csv.reader(codecs.iterdecode(file, 'utf-8'))
+    return csv_text
 
 if __name__ == "__main__":
-    location = "London"
+    location = "NewYork"
     address_full = "46 Victoria Riverside, Leeds"
-    file_ingestion(loc=location)
+    output = get_files(loc=location)
+    csv_data = file_ingestion(output)
+
+    path = f'/home/kcorb/Documents/Projects/solar-analysis-test-output/{location}.csv'
+    with open(path, 'w') as f:
+        writer = csv.writer(f)
+
+        for i in csv_data:
+            writer.writerow(i)
+
+        print(f'Output created for {location}.csv')
