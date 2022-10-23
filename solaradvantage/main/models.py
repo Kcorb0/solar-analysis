224936a5a30beabcb0dae7a48729e10232f9e17f
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-10-23 23:10:35 +0100

    Data can now be sent to the new wether db

--- solaradvantage/main/models.py
+++ solaradvantage/main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class WeatherData(models.Model):
+    """
+    This model will create a table call WeatherData which will house
+    all the rows of weather data by location.
+    """
+
+    namedate_id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    datetime = models.DateField()
+    tempmax = models.FloatField()
+    tempmin = models.FloatField()
+    temp = models.FloatField()
+    feelslikemax = models.FloatField()
+    feelslikemin = models.FloatField()
+    feelslike = models.FloatField()
+    dew = models.FloatField()
+    humidity = models.FloatField()
+    precip = models.FloatField()
+    precipprob = models.IntegerField()
+    precipcover = models.FloatField()
+    preciptype = models.CharField(max_length=255)
+    snow = models.FloatField()
+    snowdepth = models.FloatField()
+    windgust = models.FloatField()
+    windspeed = models.FloatField()
+    winddir = models.FloatField()
+    sealevelpressure = models.FloatField()
+    cloudcover = models.FloatField()
+    visibility = models.FloatField()
+    solarradiation = models.FloatField()
+    solarenergy = models.FloatField()
+    uvindex = models.IntegerField()
+    severerisk = models.CharField(max_length=255)
+    sunrise = models.DateTimeField()
+    sunset = models.DateTimeField()
+    moonphase = models.FloatField()
+    conditions = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    icon = models.CharField(max_length=255)
+    stations = models.CharField(max_length=255)
